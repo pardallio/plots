9d8871b92e21965d743e2cde59b4ac0db5b23e80
Pardallio <pardalmiguel@hotmail.com>
2022-12-09 01:40:55 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ import mock
 sys.path.insert(0, os.path.abspath('../../osirispy'))
 sys.path.insert(0, os.path.abspath('../..'))
 
-MOCK_MODULES = ['numpy']
+MOCK_MODULES = ['numpy',"h5py"]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 # Configuration file for the Sphinx documentation builder.
